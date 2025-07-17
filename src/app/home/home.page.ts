cbc4bbae7dd9c90595ba2bdc65ecb3f747bcb1d1
icg00048 <114912184+icg00048@users.noreply.github.com>
2025-07-17 15:29:49 +0200

    feat(home): Pinterest-style masonry grid list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import {
-  IonHeader,
-  IonToolbar,
-  IonContent,
-  IonSearchbar,
-  IonList,
-  IonItem
-} from '@ionic/angular/standalone';
-
+import { IonHeader, IonToolbar, IonContent, IonSearchbar, InfiniteScrollCustomEvent } from '@ionic/angular/standalone';
 import { Item } from '../model/item.model';
 import { ItemService } from '../service/item/item.service';
+import { ItemCardComponent } from '../shared/item-card/item-card.component';
+import { catchError, finalize } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -25,29 +19,83 @@ import { ItemService } from '../service/item/item.service';
     IonToolbar,
     IonContent,
     IonSearchbar,
-    IonList,
-    IonItem
+    ItemCardComponent
   ],
 })
 export class HomePage {
   private itemService = inject(ItemService);
+  public isLoading = false;
+  private currentPage = 1;
+  private error = null;
+
+
   items: Item[] = [];
   searchTerm = '';
 
-  ngOnInit() {
+  constructor() {
     this.loadItems();
   }
 
-  loadItems() {
-    this.itemService
-      .getItems()
+  loadItems(event?: InfiniteScrollCustomEvent) {
+    this.error = null;
+
+    if(!event){
+      this.isLoading = true;
+    }
+
+    this.itemService.getItems(this.currentPage)
+      .pipe(
+        finalize(() => {
+          this.isLoading = false;
+          if(event){
+            event.target.complete();
+          }
+        }),
+        catchError((err: any) => {
+          console.log(err);
+          this.error = err.error.status_message;
+          return [];
+        })
+      )
       .subscribe(list => this.items = list);
+  }
+
+  loadMore(event: InfiniteScrollCustomEvent){
+
   }
 
   get filteredItems(): Item[] {
     const term = this.searchTerm.trim().toLowerCase();
-    return term
-      ? this.items.filter(i => i.nickname.toLowerCase().includes(term))
-      : this.items;
+    if (!term) {
+      return this.items;
+    }
+  
+    return this.items.filter(item => {
+      const {
+        nickname,
+        type,
+        primaryColor,
+        secondaryColor = '',
+        pattern,
+        material = '',
+        subcategory = '',
+        occasion = '',
+        season = ''
+      } = item;
+  
+      const haystack = [
+        nickname,
+        type,
+        primaryColor,
+        secondaryColor,
+        pattern,
+        material,
+        subcategory,
+        occasion,
+        season
+      ].map(val => val.toString().toLowerCase()).join(' ');
+  
+      return haystack.includes(term);
+    });
   }
 }
