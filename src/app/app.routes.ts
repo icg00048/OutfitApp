7a83c4812b0d2b8d0f1a2f02a648551800e5af5f
icg00048 <114912184+icg00048@users.noreply.github.com>
2025-07-18 13:07:59 +0200

    feat(route): First routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'tabs',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.page').then(m => m.HomePage)
+      },
+      {
+        path: '',
+        redirectTo: '/tabs/home',
+        pathMatch: 'full'
+      }
+    ]
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
+    redirectTo: '/tabs/home',
+    pathMatch: 'full'
+  }
 ];
